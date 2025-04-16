23153b1864000ffa0963c120d79c46236664d160
Akarshl <120399764+Akarshl@users.noreply.github.com>
2025-04-16 03:53:47 +0000

    adding ipynb file with all the codes

--- PE02-Introduction_to_Python_Part2/Adding_numbers.py
+++ PE02-Introduction_to_Python_Part2/Adding_numbers.py
@@ -1,7 +1,7 @@
 try:
     num1 = int(input("Enter the first number: "))
     num2 = int(input("Enter the second number: "))
-    result = num1 + num2
+    result = num1 + num2 #sum of 2 numbers
     print("The sum is:", result)
 except ValueError:
     print("Please enter valid numbers only. Text is not allowed.")
